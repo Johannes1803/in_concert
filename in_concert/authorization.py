68a9871c048bcb0e9281d807dc9f358305fd8a10
Johannes Birk <johannes.birk@msg.group>
2023-08-11 21:20:40 +0200

    feat: get http credentials from cookie

--- in_concert/authorization.py
+++ in_concert/authorization.py
@@ -1,9 +1,26 @@
 from typing import Optional
 
-from fastapi import Request
+from fastapi import HTTPException, Request
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.security.utils import get_authorization_scheme_param
+from starlette.status import HTTP_401_UNAUTHORIZED
 
 
 class HTTPBearerWithCookie(HTTPBearer):
     async def __call__(self, request: Request) -> Optional[HTTPAuthorizationCredentials]:
-        pass
+        authorization: Optional[str] = request.cookies.get("access_token")
+        scheme, credentials = get_authorization_scheme_param(authorization)
+        if not (authorization and scheme and credentials):
+            if self.auto_error:
+                raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Not authenticated")
+            else:
+                return None
+        if scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Invalid authentication credentials",
+                )
+            else:
+                return None
+        return HTTPAuthorizationCredentials(scheme=scheme, credentials=credentials)
