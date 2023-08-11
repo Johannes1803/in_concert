2e54dba72228f25059ae22a1eaa7c7b2432d03bd
Johannes Birk <johannes.birk@msg.group>
2023-08-11 21:29:54 +0200

    test: test handling invalid/missing formats of jwt token

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,6 +1,7 @@
 from unittest import mock
 
 import pytest
+from fastapi import HTTPException
 
 from in_concert.authorization import HTTPBearerWithCookie
 
@@ -12,8 +13,38 @@ def request_obj():
     return request
 
 
+@pytest.fixture
+def request_obj_no_token():
+    request = mock.MagicMock()
+    request.cookies = {}
+    return request
+
+
+@pytest.fixture
+def request_obj_malformed_token():
+    request = mock.MagicMock()
+    request.cookies = {"acces_token": "not_bearer jwt pattern"}
+    return request
+
+
 @pytest.mark.asyncio
 async def test_get_token_from_cookie_should_return_token_if_set(request_obj):
     bearer = HTTPBearerWithCookie()
     http_credentials = await bearer(request_obj)
     assert http_credentials
+
+
+@pytest.mark.asyncio
+async def test_get_token_should_raise_401_if_cookie_not_set(request_obj_no_token):
+    bearer = HTTPBearerWithCookie()
+    with pytest.raises(HTTPException) as e:
+        _ = await bearer(request_obj_no_token)
+        assert e.status_code == 401
+
+
+@pytest.mark.asyncio
+async def test_get_token_should_raise_401_if_token_not_expected_format(request_obj_no_token):
+    bearer = HTTPBearerWithCookie()
+    with pytest.raises(HTTPException) as e:
+        _ = await bearer(request_obj_no_token)
+        assert e.status_code == 401
