a79498839c15d08e7f2e68e7a008480e657d2e80
Johannes Birk <johannes.birk@msg.group>
2023-08-13 20:58:09 +0200

    test: assert redirect, setup with authlib client

--- in_concert/routers/auth_router.py
+++ in_concert/routers/auth_router.py
@@ -1,9 +1,10 @@
+from authlib.integrations.starlette_client import OAuth
 from fastapi.routing import APIRouter
 
 from in_concert.settings import Auth0Settings
 
 
-def create_router(auth_settings: Auth0Settings):
+def create_router(auth_settings: Auth0Settings, oauth: OAuth):
     router = APIRouter()
 
     @router.get("/login")
--- tests/routers/test_auth_router.py
+++ tests/routers/test_auth_router.py
@@ -1,4 +1,7 @@
+from unittest import mock
+
 import pytest
+from authlib.integrations.starlette_client import OAuth
 from fastapi.testclient import TestClient
 
 from in_concert.routers.auth_router import create_router
@@ -7,10 +10,16 @@ from in_concert.settings import Auth0SettingsTest
 
 class TestAuthRouter:
     @pytest.fixture
-    def client(self):
-        router = create_router(Auth0SettingsTest)
+    def oauth(self):
+        oauth = OAuth()
+        oauth.auth0 = mock.MagicMock()
+        oauth.auth0.authorize_redirect = mock.MagicMock()
+
+    @pytest.fixture
+    def client(self, oauth):
+        router = create_router(Auth0SettingsTest, oauth=oauth)
         return TestClient(router)
 
     def test_login(self, client):
         response = client.get("/login")
-        assert response.status_code
+        assert response.status_code == 302
