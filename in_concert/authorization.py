9d94f83ed3661ea11cb4e37a142cc76a86f1413d
Johannes Birk <johannes.birk@msg.group>
2023-08-13 13:42:45 +0200

    test: add token verifier boilerplate

--- in_concert/authorization.py
+++ in_concert/authorization.py
@@ -1,5 +1,6 @@
-from typing import Optional
+from typing import Callable, Optional
 
+import jwt
 from fastapi import HTTPException, Request
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from fastapi.security.utils import get_authorization_scheme_param
@@ -8,6 +9,13 @@ from starlette.status import HTTP_401_UNAUTHORIZED
 
 class HTTPBearerWithCookie(HTTPBearer):
     async def __call__(self, request: Request) -> Optional[HTTPAuthorizationCredentials]:
+        """
+        Get the credentials from cookies.
+
+        :param request: The request to get the token from
+        :return: jwt bearer token
+        :raises HTTPException: If the token is not set or format is invalid
+        """
         authorization: Optional[str] = request.cookies.get("access_token")
         scheme, credentials = get_authorization_scheme_param(authorization)
         if not (authorization and scheme and credentials):
@@ -24,3 +32,22 @@ class HTTPBearerWithCookie(HTTPBearer):
             else:
                 return None
         return HTTPAuthorizationCredentials(scheme=scheme, credentials=credentials)
+
+
+class JwkTokenVerifier:
+    """Does all the token verification using PyJWT"""
+
+    def __init__(self, settings, jwks_client: jwt.PyJWKClient, decoder: Callable = jwt.decode):
+        self.settings = settings
+        self.jwks_client = jwks_client
+        self.decoder = decoder
+
+    def verify(self, token: str) -> dict:
+        """
+        Verifies the token and returns the payload if successful.
+        If unsuccessful, returns a dict with a status code.
+        param: token: The token to verify
+        return: dict: The payload if successful
+        raises: jwt.exceptions.DecodeError: If the token is invalid
+        """
+        pass
