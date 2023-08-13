64de090d4e8de0f602acace177a8b1042ca03a40
Johannes Birk <johannes.birk@msg.group>
2023-08-13 13:41:47 +0200

    test: group tests in classes

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -6,45 +6,41 @@ from fastapi import HTTPException
 from in_concert.authorization import HTTPBearerWithCookie
 
 
-@pytest.fixture
-def request_obj():
-    request = mock.MagicMock()
-    request.cookies = {"access_token": "bearer valid_jwt_token"}
-    return request
+class TestHTTPBearerWithCookie:
+    @pytest.fixture
+    def request_obj(self):
+        request = mock.MagicMock()
+        request.cookies = {"access_token": "bearer valid_jwt_token"}
+        return request
 
+    @pytest.fixture
+    def request_obj_no_token(self):
+        request = mock.MagicMock()
+        request.cookies = {}
+        return request
 
-@pytest.fixture
-def request_obj_no_token():
-    request = mock.MagicMock()
-    request.cookies = {}
-    return request
+    @pytest.fixture
+    def request_obj_malformed_token(self):
+        request = mock.MagicMock()
+        request.cookies = {"acces_token": "not_bearer jwt pattern"}
+        return request
 
+    @pytest.mark.asyncio
+    async def test_get_token_from_cookie_should_return_token_if_set(self, request_obj):
+        bearer = HTTPBearerWithCookie()
+        http_credentials = await bearer(request_obj)
+        assert http_credentials
 
-@pytest.fixture
-def request_obj_malformed_token():
-    request = mock.MagicMock()
-    request.cookies = {"acces_token": "not_bearer jwt pattern"}
-    return request
+    @pytest.mark.asyncio
+    async def test_get_token_should_raise_401_if_cookie_not_set(self, request_obj_no_token):
+        bearer = HTTPBearerWithCookie()
+        with pytest.raises(HTTPException) as e:
+            _ = await bearer(request_obj_no_token)
+            assert e.status_code == 401
 
-
-@pytest.mark.asyncio
-async def test_get_token_from_cookie_should_return_token_if_set(request_obj):
-    bearer = HTTPBearerWithCookie()
-    http_credentials = await bearer(request_obj)
-    assert http_credentials
-
-
-@pytest.mark.asyncio
-async def test_get_token_should_raise_401_if_cookie_not_set(request_obj_no_token):
-    bearer = HTTPBearerWithCookie()
-    with pytest.raises(HTTPException) as e:
-        _ = await bearer(request_obj_no_token)
-        assert e.status_code == 401
-
-
-@pytest.mark.asyncio
-async def test_get_token_should_raise_401_if_token_not_expected_format(request_obj_no_token):
-    bearer = HTTPBearerWithCookie()
-    with pytest.raises(HTTPException) as e:
-        _ = await bearer(request_obj_no_token)
-        assert e.status_code == 401
+    @pytest.mark.asyncio
+    async def test_get_token_should_raise_401_if_token_not_expected_format(self, request_obj_no_token):
+        bearer = HTTPBearerWithCookie()
+        with pytest.raises(HTTPException) as e:
+            _ = await bearer(request_obj_no_token)
+            assert e.status_code == 401
