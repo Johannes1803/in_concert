88efcba33924ca0c269ecefa5be75b16c7f1e39d
Johannes Birk <johannes.birk@msg.group>
2023-08-15 19:23:34 +0200

    feat: save token in cookie

--- in_concert/routers/auth_router.py
+++ in_concert/routers/auth_router.py
@@ -1,5 +1,6 @@
 from authlib.integrations.starlette_client import OAuth
 from fastapi import Request
+from fastapi.responses import RedirectResponse
 from fastapi.routing import APIRouter
 
 from in_concert.settings import Auth0Settings
@@ -22,8 +23,12 @@ def create_router(auth_settings: Auth0Settings, oauth: OAuth):
         redirect_uri = request.url_for("auth")
         return await oauth.auth0.authorize_redirect(request, redirect_uri)
 
-    @router.get("/auth")
+    @router.get("/callback")
     async def auth(request: Request):
-        pass
+        token = await oauth.auth0.authorize_access_token(request)
+
+        response = RedirectResponse(url="/")
+        response.set_cookie(key="access_token", value=f'Bearer {token.get("access_token")}', httponly=True)
+        return response
 
     return router
