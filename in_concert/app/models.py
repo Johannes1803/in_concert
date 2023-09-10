b10a4df705fec81f2ed679080ce8dd14cc346aa5
johannes <johannes.birk11@web.de>
2023-09-10 21:14:18 +0200

    docs: add docstrings for orm models

--- in_concert/app/models.py
+++ in_concert/app/models.py
@@ -1,3 +1,4 @@
+"""Models representing the database tables."""
 from sqlalchemy import Column, Engine, Integer, String
 from sqlalchemy.orm import DeclarativeBase, Session
 
@@ -7,12 +8,20 @@ class Base(DeclarativeBase):
 
 
 class Venue(Base):
+    """A venue where a concert is held."""
+
     __tablename__ = "venues"
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     city = Column(String(120), nullable=False)
 
     def insert(self, engine: Engine) -> int:
+        """
+        Insert a new venue into the database.
+
+        param engine: The SQLAlchemy engine to use.
+        return: The ID of the newly inserted venue.
+        """
         with Session(engine) as session:
             session.add(self)
             session.commit()
