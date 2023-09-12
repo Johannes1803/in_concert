b52be99f4cb69a26afcf7228c241937050a22461
Johannes Birk <johannes.birk@msg.group>
2023-09-12 16:32:12 +0200

    test: test access to private endpoint in bdd style

---
+++ tests/features/access_private_data.feature
@@ -0,0 +1,15 @@
+Feature: Access protected information
+
+  @fixture.settings_auth
+  @fixture.test_client
+  Scenario: Access protected information as logged-in user
+     Given I am logged in as a user
+      When I navigate to the private route
+      Then I get access to private data
+
+  @fixture.settings_auth
+  @fixture.test_client
+  Scenario: Access protected information as anonymous user
+     Given I am not logged in as a user
+      When I navigate to the private route
+      Then I get access to private data denied
---
+++ tests/features/steps/access_private_data.py
@@ -0,0 +1,34 @@
+from behave import given, then, when
+from behave.runner import Context
+
+from tests.setup import get_bearer_token
+
+
+@given("I am logged in as a user")
+def logged_in_precondition(context: Context):
+    token = get_bearer_token(settings_auth=context.settings_auth)
+    context.cookie = {"access_token": f'Bearer {token["access_token"]}'}
+
+
+@given("I am not logged in as a user")
+def not_logged_in_precondition(context: Context):
+    context.cookie = None
+
+
+@when("I navigate to the private route")
+def navigate_to_private_action(context: Context):
+    response = context.test_client.get("/private", cookies=context.cookie)
+    context.response = response
+
+
+@then("I get access to private data")
+def access_private_data_outcome(context: Context):
+    assert context.response
+    assert context.response.status_code == 200
+    assert context.response.json()
+
+
+@then("I get access to private data denied")
+def access_private_data_denied_outcome(context: Context):
+    assert context.response
+    assert context.response.status_code == 401
