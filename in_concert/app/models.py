b3fd6e3af6456eb12a6157bde0d1f9f70bd26d49
johannes <johannes.birk11@web.de>
2023-09-22 20:35:08 +0200

    feat: add user model

---
+++ in_concert/app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "user_account"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    sub: Mapped[str] = mapped_column(String(30))
