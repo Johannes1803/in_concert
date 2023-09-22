6175b37bb90d81baf3588b60612977d73c5ed16c
johannes <johannes.birk11@web.de>
2023-09-22 21:22:38 +0200

    feat: insert user into db

--- in_concert/app/models.py
+++ in_concert/app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -10,3 +10,14 @@ class User(Base):
     __tablename__ = "user_account"
     id: Mapped[int] = mapped_column(primary_key=True)
     sub: Mapped[str] = mapped_column(String(30))
+
+    def insert(self, engine) -> int:
+        """Inserts a user into the database and returns the user's id.
+        param: engine: a SQLAlchemy engine
+        return: the user's id
+        """
+        with Session(engine) as session:
+            session.add(self)
+            session.commit()
+            session.refresh(self)
+            return self.id
