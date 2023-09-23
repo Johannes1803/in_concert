547e413609df79ed0820cda8d45a3319aea2c2ba
johannes <johannes.birk11@web.de>
2023-09-23 20:00:07 +0200

    lint: supress false positive flake8

--- in_concert/app/__init__.py
+++ in_concert/app/__init__.py
@@ -1,2 +1,3 @@
 """Package containing the main application logic."""
-from in_concert.app.app_factory import create_app, get_db_session_factory
+
+from in_concert.app.app_factory import create_app, get_db_session_factory  # noqa: F401
