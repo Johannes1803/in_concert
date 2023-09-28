c9d3c9eaafca786b4b9450bf4ba122a302f14660
johannes <johannes.birk11@web.de>
2023-09-28 19:16:11 +0200

    chore: fix deprecation warning

--- tests/unit/routers/auth/test_models.py
+++ tests/unit/routers/auth/test_models.py
@@ -9,7 +9,7 @@ def test_insert_user_should_add_user_to_db(db_session: Session) -> None:
     user_id: int = user.insert(session=db_session)
 
     with db_session:
-        user_from_db = db_session.query(User).get(user_id)
+        user_from_db = db_session.get(User, user_id)
     assert user_from_db
     assert user_from_db.id
     assert user_from_db.id == "oauth2|1234567890"
