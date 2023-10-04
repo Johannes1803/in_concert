fc6cb0ad99634c37f07d30f5c4bf77c2b6675e0e
johannes <johannes.birk11@web.de>
2023-10-04 18:00:39 +0200

    chore: update alembic

--- in_concert/app/schemas.py
+++ in_concert/app/schemas.py
@@ -5,3 +5,16 @@ class UserSchema(BaseModel):
     id: str
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class VenueSchema(BaseModel):
+    name: str
+    address: str
+    state: str
+    zip_code: int
+    phone: int
+    website: str
+    image_link: str
+    genres: str
+
+    model_config = ConfigDict(from_attributes=True)
