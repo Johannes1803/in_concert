50a34f54d7257c686e64aec5fbe323ce23779069
johannes <johannes.birk11@web.de>
2023-12-01 16:24:29 +0100

    chore: improve variable name

--- in_concert/app/main.py
+++ in_concert/app/main.py
@@ -2,12 +2,12 @@ import uvicorn
 from sqlalchemy import Engine, create_engine
 
 from in_concert.app import create_app
-from in_concert.settings import Auth0SettingsDev
+from in_concert.settings import AppSettingsDev
 
 if __name__ == "__main__":
-    auth_settings = Auth0SettingsDev()
-    engine: Engine = create_engine(auth_settings.db_connection_string.get_secret_value())
-    app = create_app(auth_settings, engine=engine)
+    app_settings_dev = AppSettingsDev()
+    engine: Engine = create_engine(app_settings_dev.db_connection_string.get_secret_value())
+    app = create_app(app_settings_dev, engine=engine)
     uvicorn.run(
         app,
         host="localhost",
